ee5fddbb7573ecc0b49f57c3a2e4f941a9676865
GIlles Souton <gilles.souton@etu.u-bordeaux.fr>
2022-01-30 14:07:25 +0100

    added first_load to init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,9 @@
 pcall(require, "impatient")
 
+if require "first_load"() then
+    return
+end
+
 require("configuration") -- basic editor settings
 require("packer_plugins") -- package manager and plugins
 require("plugins.configuration") -- configure each plugin
